76bc17c9d2560f0de408b19cade02dac85d14e13
Tushar Chaudhari <chaudharitushar9999@gmail.com>
2025-11-20 20:53:50 +0530

    Resolved Infinite Bidirectional JSON Serialization Loop
    Resolved Infinite Bidirectional JSON Serialization Loop:
    Jackson (JSON serializer used by Spring Boot) tries to convert both sides → causing infinite recursion.
    
    ✔ We must fix this using one annotation
    
    We need to tell Jackson:
    
    “Do not serialize child’s back-reference to parent again.”
    
    We always solve this using:
    (Recommended)
    @JsonManagedReference + @JsonBackReference

--- src/main/java/com/shopease/backend/entity/OrderItem.java
+++ src/main/java/com/shopease/backend/entity/OrderItem.java
@@ -1,6 +1,14 @@
 package com.shopease.backend.entity;
 
-import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name="order_items")
@@ -13,6 +21,7 @@ public class OrderItem
 	// Order reference
 	@ManyToOne
 	@JoinColumn(name="order_id")
+	@JsonBackReference
 	private Order order;
 	
 	// product ref
